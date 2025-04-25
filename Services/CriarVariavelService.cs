2afba3041e192a88c2b16d9f04371b547bb618b6
Gubio Garcia <gubiogarcia@gmail.com>
2025-04-25 12:53:28 -0300

    feat: tratamento para entrada com variáveis de mesmo nome

--- Services/CriarVariavelService.cs
+++ Services/CriarVariavelService.cs
@@ -5,19 +5,48 @@ namespace CriarVariavel.Services
 {
     public class CriarVariavelService
     {
+        private int contadorVariavel = 0;
+
         public string CriarVariavel(List<Variavel> variaveis)
         {
+            if(variaveis == null || variaveis.Count == 0) throw new Exception("Nenhuma variável foi informada.");
+
+            if (!ValidaDuplicidadeVariavel(variaveis)) RenomearVariavel(variaveis);
+
             List<Dictionary<string, string>> variavel = [];
-            foreach (var valoresVariavel in variaveis)
+            foreach (Variavel _valoresVariavel in variaveis)
             {
                 Dictionary<string, string> variavelItem = new()
                 {
-                    { valoresVariavel.NomeVariavel, valoresVariavel.ValorVariavel }
+                    { _valoresVariavel.NomeVariavel, _valoresVariavel.ValorVariavel }
                 };
                 variavel.Add(variavelItem);
             }
 
             return JsonSerializer.Serialize(variavel, new JsonSerializerOptions { WriteIndented = true });
+        }
+
+        private bool ValidaDuplicidadeVariavel(List<Variavel> variaveis)
+        {
+            HashSet<string> _variaveisUnicas = [];
+            foreach (Variavel _variavel in variaveis)
+            {
+                if (!_variaveisUnicas.Add(_variavel.NomeVariavel)) return false; // Encontrado variáveis duplicadas 
+            }
+            return true;
+        }
+
+        private void RenomearVariavel(List<Variavel> variaveis)
+        {
+            HashSet<string> _variaveisAux = [];
+            foreach (Variavel _variavel in variaveis)
+            {
+                if(!_variaveisAux.Add(_variavel.NomeVariavel))
+                {
+                    _variavel.NomeVariavel += contadorVariavel;
+                    contadorVariavel++;
+                }
+            }
         }
     }
 }
